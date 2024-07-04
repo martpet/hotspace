54717203c45b73e8161e4dcc757c7be3821c5aee
Martin Petrov <martin_petrov@me.com>
2024-07-04 15:13:37 +0300

    Thu Jul  4 15:13:37 EEST 2024

--- routes/space.ts
+++ routes/space.ts
@@ -1,6 +1,6 @@
 import type { Context } from "../lib/types.ts";
 
-export default function spacepage(ctx: Context) {
+export default function space(ctx: Context) {
   const { spacename } = ctx.patternResult!.hostname.groups;
   return `<h1>${spacename}</h1>`;
 }
