4d11eebc66ebd588a319d84f9ff697534f847325
Martin Petrov <martin_petrov@me.com>
2024-12-21 20:31:42 +0200

    remove login page from home

--- handlers/home.tsx
+++ handlers/home.tsx
@@ -1,22 +1,25 @@
 import ButtonCreateDir from "../snippets/inodes/ButtonCreateDir.tsx";
 import SpacesList from "../snippets/inodes/SpacesList.tsx";
-import LoginPage from "../snippets/pages/LoginPage.tsx";
 import Page from "../snippets/pages/Page.tsx";
 import { listRootDirsByOwner } from "../util/kv/inodes.ts";
-import type { AppContext } from "../util/types.ts";
+import type { AppContext, DirNode } from "../util/types.ts";
 
 export default async function homeHandler(ctx: AppContext) {
   const user = ctx.state.user;
 
-  if (!user) {
-    return <LoginPage title="HotSpace" />;
-  }
-
   return (
     <Page siteNameIsHeading>
-      <h2>Your spaces</h2>
-      <SpacesList dirs={await listRootDirsByOwner(user.id)} />
-      <ButtonCreateDir isRootDir />
+      {user && <UserHome spaces={await listRootDirsByOwner(user.id)} />}
     </Page>
+  );
+}
+
+function UserHome(props: { spaces: DirNode[] }) {
+  return (
+    <>
+      <h2>Your spaces</h2>
+      <SpacesList dirs={props.spaces} />
+      <ButtonCreateDir isRootDir />
+    </>
   );
 }
