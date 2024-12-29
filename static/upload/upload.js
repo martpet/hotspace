17164889419870611a75ec9737676c034d14d8b6
Martin Petrov <martin_petrov@me.com>
2024-12-29 10:24:18 +0200

    wip: upload fix input field

--- static/upload/upload.js
+++ static/upload/upload.js
@@ -1,8 +1,8 @@
-const fileInput = document.querySelector("input[type=file]");
+const input = document.querySelector("input[type=file]");
 const { workerPath } = input.dataset;
 const worker = new Worker(workerPath, { type: "module" });
 
 input.addEventListener("change", () => {
-  const file = fileInput.files[0];
+  const file = input.files[0];
   worker.postMessage(file);
 });
