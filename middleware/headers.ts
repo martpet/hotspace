90529c949fdf2fd7158bfa7dd5cec9945beebc76
Martin Petrov <martin_petrov@me.com>
2025-03-08 09:19:30 +0200

    fix frame csp

--- middleware/headers.ts
+++ middleware/headers.ts
@@ -23,7 +23,7 @@ export const headersMiddleware: AppMiddleware = async (ctx, next) => {
     `default-src 'self' 'nonce-${ctx.scpNonce}' ${ASSETS_CLOUDFRONT_URL} ${INODES_CLOUDFRONT_URL}`,
     `worker-src 'self' blob:`,
     `media-src 'self' blob: ${INODES_CLOUDFRONT_URL}`,
-    `frame-src 'self' ${INODES_CLOUDFRONT_URL}`,
+    `frame-src 'self' ${ASSETS_CLOUDFRONT_URL}`,
   ].join(";");
 
   const docxIframeCsp =
