67828aa0c0cbe1d4f4ecda51d1bbf9f50bacc93b
Martin Petrov <martin_petrov@me.com>
2025-03-09 17:18:41 +0200

    start preloading docx scripts early

--- snippets/inodes/file_preview/DocxPreview.tsx
+++ snippets/inodes/file_preview/DocxPreview.tsx
@@ -7,7 +7,6 @@ interface Props {
 
 export default function DocxPreview(props: Props, ctx: AppContext) {
   const { fileNodeUrl } = props;
-
   const src = asset("docx/iframe.html");
   const url = new URL(src, URL.canParse(src) ? undefined : ctx.url.origin);
 
@@ -15,5 +14,19 @@ export default function DocxPreview(props: Props, ctx: AppContext) {
   url.searchParams.set("jszipPath", asset("docx/jszip.min.js"));
   url.searchParams.set("docxPreviewPath", asset("docx/docx-preview.min.js"));
 
-  return <iframe id="docx-iframe" src={url.href} />;
+  return (
+    <>
+      <link
+        rel="preload"
+        href={asset("docx/jszip.min.js")}
+        as="script"
+      />
+      <link
+        rel="preload"
+        href={asset("docx/docx-preview.min.js")}
+        as="script"
+      />
+      <iframe id="docx-iframe" src={url.href} />
+    </>
+  );
 }
