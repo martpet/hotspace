34d4838af42ce7beba1343c92e3ed952d06dd379
Martin Petrov <martin_petrov@me.com>
2025-05-11 20:21:04 +0300

    refactor: remove saving subscriber by user id

--- util/kv/push_subscribers.ts
+++ util/kv/push_subscribers.ts
@@ -3,34 +3,15 @@ import { kv } from "./kv.ts";
 
 export const keys = {
   byId: (subscriberId: string) => ["subscribers", subscriberId],
-  byUserId: (
-    userId: string,
-    subscriberId: string,
-  ) => ["subscribers_by_user", userId, subscriberId],
 };
 
 export function setSubscriber(
   subscriber: PushSubscriber,
   atomic = kv.atomic(),
 ) {
-  atomic.set(keys.byId(subscriber.id), subscriber);
-  if (subscriber.userId) {
-    atomic.set(keys.byUserId(subscriber.userId, subscriber.id), subscriber);
-  }
-  return atomic;
+  return atomic.set(keys.byId(subscriber.id), subscriber);
 }
 
 export function getSubscriber(subscriberId: string) {
   return kv.get<PushSubscriber>(keys.byId(subscriberId));
-}
-
-export async function listSubscribersByUserId(userId: string) {
-  const iter = kv.list<PushSubscriber>({
-    prefix: keys.byUserId(userId, "").slice(0, -1),
-  });
-  return (await Array.fromAsync(iter)).map(({ value }) => value);
-}
-
-export function deleteSubscriberByUserId(userId: string, subscriberId: string) {
-  return kv.delete(keys.byUserId(userId, subscriberId));
 }
