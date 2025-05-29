90ea459d08e4731bb8e162d3a944be1dae4eafed
Martin Petrov <martin_petrov@me.com>
2025-05-29 21:56:28 +0300

    ui: show iso time in `<RelativeTime>` title attr

--- components/RelativeTime.tsx
+++ components/RelativeTime.tsx
@@ -14,10 +14,5 @@ export default function RelativeTime(props: Props, ctx: AppContext) {
   const date = props.date || new Date(decodeTime(props.uuid));
   const text = getRelativeTime(date, ctx.locale);
 
-  const dateFmt = new Intl.DateTimeFormat(ctx.locale, {
-    dateStyle: "long",
-    timeStyle: "medium",
-  });
-
-  return <time title={dateFmt.format(date)}>{text}</time>;
+  return <time title={date.toISOString()}>{text}</time>;
 }
